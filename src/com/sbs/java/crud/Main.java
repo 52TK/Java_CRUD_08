2bff194f0eb96e134a42ee34bc30a5de4e8d1966
52TK <0910ydn@gmail.com>
2021-08-26 18:09:17 +0900

    게시물 작성시 작성날짜 저장

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -26,12 +26,13 @@ public class Main {
 			if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
+				String regDate = Util.getNowDateStr();  // Util 파일에서 불러옴
 				System.out.println("제목 : ");
 				String title = sc.nextLine();
 				System.out.println("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 				
 				System.out.printf("%d번 글이 생성되었습니다.\n",id);
@@ -67,11 +68,13 @@ public class Main {
 					continue;
 				} 
 				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : 2021-12-12 12:12:12\n");
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
-				System.out.printf("제목 : %s\n", foundArticle.body);
+				System.out.printf("내용 : %s\n", foundArticle.body);
 				
 			}
+			
+			
 			
 			else if (command.startsWith("article delete ")) {
 				String[] commandBits = command.split(" ");
@@ -114,11 +117,13 @@ public class Main {
 }
 class Article{
 	int id;
+	String regDate;
 	String title;
 	String body;
 	
-	public Article(int id,String title,String body) {
+	public Article(int id, String regDate, String title,String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
---
+++ src/com/sbs/java/crud/Util.java
@@ -0,0 +1,14 @@
+package com.sbs.java.crud;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	public static String getNowDateStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		Date time = new Date();
+				
+		return format.format(time);
+	}
+}
