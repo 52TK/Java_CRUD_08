05560b9a434b529d0f7531dc32004e5be12492b3
52TK <0910ydn@gmail.com>
2021-08-26 18:46:43 +0900

    게시물 조회수 기능

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -41,11 +41,11 @@ public class Main {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
-				System.out.println("번호 / 제목");
+				System.out.println("번호   /   조회   /   제목");
 				for (int i = articles.size() - 1; i >= 0 ; i-- ) {
 					Article article = articles.get(i);
 					
-					System.out.printf("%d  /  %s\n", article.id, article.title);
+					System.out.printf("%d  /  %d   /   %s\n", article.id, article.hit, article.title);
 				}
 				
 			} else if (command.startsWith("article detail ")) {
@@ -66,11 +66,15 @@ public class Main {
 				if(foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
-				} 
+				}
+				
+				foundArticle.increaseHit();
+				
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+				System.out.printf("조회 : %d\n", foundArticle.hit);
 				
 			}
 			
@@ -152,11 +156,17 @@ class Article{
 	String regDate;
 	String title;
 	String body;
+	int hit;
 	
 	public Article(int id, String regDate, String title,String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.hit = 0;
+	}
+	
+	public void increaseHit() {
+		hit++;
 	}
 }
