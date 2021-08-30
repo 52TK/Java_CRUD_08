b3c9d6065fc8135746662f1884916c6ab2bed952
52TK <0910ydn@gmail.com>
2021-08-30 18:45:13 +0900

    특정 게시물을 찾는 메서드 구현 후 관련된 중복 제거

--- src/com/sbs/java/crud/App.java
+++ src/com/sbs/java/crud/App.java
@@ -3,7 +3,6 @@ package com.sbs.java.crud;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import com.sbs.java.crud.dto.Article;
 import com.sbs.java.crud.util.Util;
 
@@ -16,11 +15,8 @@ public class App {
 
 	public void run() {
 		System.out.println("==프로그램 시작==");
-
 		makeTestData();
-
 		Scanner sc = new Scanner(System.in);
-
 		while (true) {
 			System.out.printf("명령어) ");
 			String command = sc.nextLine().trim();
@@ -37,10 +33,8 @@ public class App {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-
 				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
-
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else if (command.equals("article list")) {
 				if (articles.size() == 0) {
@@ -50,32 +44,18 @@ public class App {
 				System.out.println("번호  |  조회 | 제목");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-
 					System.out.printf("%4d | %4d | %s\n", article.id, article.hit, article.title);
 				}
-
 			} else if (command.startsWith("article detail ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]); // "1" -> 1
-
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) { // 0 1 2 3 4
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-
 				foundArticle.increaseHit();
-
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
@@ -84,46 +64,24 @@ public class App {
 			} else if (command.startsWith("article modify ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]); // "1" -> 1
-
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) { // 0 1 2 3 4
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-
 				System.out.printf("새 제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("새 내용 : ");
 				String body = sc.nextLine();
-
 				foundArticle.title = title;
 				foundArticle.body = body;
-
 				System.out.printf("%d번 게시물이 수정되었습니다.\n", foundArticle.id);
 			} else if (command.startsWith("article delete ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
-
-				int foundIndex = -1;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundIndex = i;
-						break;
-					}
-				}
+				
+				int foundIndex = getArticleIndexById(id);
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -135,15 +93,48 @@ public class App {
 				articles.remove(foundIndex);
 				System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
 			}
-
 			else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
 			}
 		}
-
 		sc.close();
-
 		System.out.println("==프로그램 끝==");
+	}
+
+	private int getArticleIndexById(int id) {
+		int i = 0;
+		for (Article article : articles) {
+			if (article.id == id) {
+				return i;
+			}
+			i++;
+		}
+
+		return -1;
+	}
+
+	private Article getArticleById(int id) {
+//		for (int i = 0; i < articles.size(); i++) {
+//			// size -> 10 0 ... 9
+//			Article article = articles.get(i);
+//
+//			if (article.id == id) {
+//				return article;
+//			}
+//		}
+		// 위의 반복문과 동일한 일을 하는 향상된 for문
+//		for (Article article : articles) {
+//			if (article.id == id) {
+//				return article;
+//			}
+//		}
+
+		int index = getArticleIndexById(id);
+
+		if (index != -1) {
+			return articles.get(index);
+		}
+		return null;
 	}
 
 	private void makeTestData() {
@@ -153,5 +144,4 @@ public class App {
 		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 22));
 		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 33));
 	}
-
 }
